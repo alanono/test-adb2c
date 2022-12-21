03ec9c5a1fff640b83f426dbd089dad8813180ae
alanono <alan2ono@gmail.com>
2022-12-21 15:38:33 -0300

    test
    test

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { Provider } = require('oidc-provider');
+const path = require('path');
+const app = express();
+//Middlewares
+app.use(express.static(__dirname + '/public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+const configuration = {
+  clients: [{
+    client_id: "827231e9-9e10-4cd9-9b70-0886687023de",
+    client_secret: "66546A576E5A7234753778214125442A472D4B6150645367556B587032733576",
+    grant_types: ["authorization_code"],
+    redirect_uris: ["https://oidcdebugger.com/debug"],
+    response_types: ["code",],
+  }],
+  pkce: {
+    required: () => false,
+  },
+};
+const oidc = new Provider('http://localhost:3000', configuration);
+app.use("/oidc",oidc.callback());
+app.listen(3000, function () {
+  console.log('OIDC is listening on port 3000!');
+});
